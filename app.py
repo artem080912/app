d7ebcf619b7e5275c352b0f8ec7dad88698a3074
Artem Abernikhin <irina.abernihina@gmail.com>
2025-01-13 20:49:03 +0200

    modified:   app.py 	new file:   ftypes.py

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ import json
 import  random
 import  tkinter as tk
 import recommand as r
+import ftypes as f
 from  tkinter import messagebox
-
-root = tk.Tk()
-root.title("Рекомендаційний фільм!")
-
-with open("data.json","r",encoding="utf-8") as file:
-    data = json.load(file)
-
-genres = ['бойовик', 'трилер', 'фантастика', 'пригоди', 'драма', 'кримінал', 'музика',
-          'жахи', 'комедія', 'фентезі', 'військовий', 'документальний', 'детектив', 'сімейний',
-          'мелодрама', 'мультфільм', 'біографія', 'аніме', 'концерт', 'вестерн', 'спорт', 'історія',
-          'мюзикл']
-
 var = tk.StringVar(value="бойовик")
 
-for genre in genres:
-    tk.Radiobutton(root,text=genre,variable=var,value=genre).pack(anchor="w")
+def main():
+    root = tk.Tk()
+    root.title("Рекомендаційний фільм!")
 
-tk.Button(root,text="Порекомендуй мені фільм",command =r.recommend_voice(var)).pack(pady=10)
+    with open("data.json","r",encoding="utf-8") as file:
+        data = json.load(file)
 
-root.mainloop()
+
+    for genre in f.genres:
+        tk.Radiobutton(root,text=genre,variable=var,value=genre).pack(anchor="w")
+
+    tk.Button(root,text="Порекомендуй мені фільм",command =r.recommend_voice(var)).pack(pady=10)
+
+    root.mainloop()
+
+main()
